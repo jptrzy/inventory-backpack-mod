3cead10335875fa93c6f3f27dc9e5863ec83369f
Jakub Trzeciak <jptrzy@gmail.com>
2022-01-12 14:10:46 +0100

    :sparkles: Working Fast Move

--- src/main/java/net/jptrzy/inventory/backpack/mixin/ScreenHandlerMixin.java
+++ src/main/java/net/jptrzy/inventory/backpack/mixin/ScreenHandlerMixin.java
@@ -1,5 +1,6 @@
 package net.jptrzy.inventory.backpack.mixin;
 
+import net.jptrzy.inventory.backpack.Main;
 import net.jptrzy.inventory.backpack.item.BackpackItem;
 import net.jptrzy.inventory.backpack.screen.BackpackScreenHandler;
 import net.minecraft.entity.player.PlayerEntity;
@@ -7,6 +8,7 @@ import net.minecraft.screen.*;
 import net.minecraft.screen.slot.SlotActionType;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -18,23 +20,26 @@ public class ScreenHandlerMixin {
         return ((ScreenHandler) (Object) this);
     }
 
-    //It works!!!
-//    @Unique
-    public boolean open_inventory = false;
+    //TODO could be a problem in the feature; may stacks could help with this
+    @Unique
+    public boolean try_open_inventory = false;
+    @Unique
+    public boolean try_open_backpack = false;
 
-    //Save Backpack Inventory
-    //TODO if problem with backpack curse, then you can update it here.
-    //TODO | or just check on pickup if not empty inventory and fast apply curse
     @Inject(at = @At("HEAD"), method = "onSlotClick", cancellable = true)
     private void HEAD_onSlotClick(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci) {
         if(player.world.isClient()){return;}
-//        if(slotIndex == -999){return;}
         if(slotIndex<0 || slotIndex>=getThis().slots.size()){return;}
-        if(!(BackpackItem.isWearingIt(player, getThis().getSlot(slotIndex).getStack()))){return;}
+        if(!(BackpackItem.isWearingIt(player, getThis().getSlot(slotIndex).getStack()) && getThis() instanceof BackpackScreenHandler )){
+            //Check if try_open_backpack for fastMove
+            if(!(getThis() instanceof BackpackScreenHandler) && getThis() instanceof PlayerScreenHandler && getThis().getSlot(slotIndex).getStack().isOf(Main.BACKPACK)){
+                try_open_backpack = true;
+                return;
+            }
+            return;
+        }
 
-        if(!(getThis() instanceof BackpackScreenHandler)){return;}
-        open_inventory = true;
-
+        try_open_inventory = true;
 
         BackpackItem.updateCurse(getThis().getSlot(slotIndex).getStack(), player);
 
@@ -52,13 +57,20 @@ public class ScreenHandlerMixin {
             return;
         }
 
-        if(open_inventory && !((BackpackScreenHandler)getThis()).getBackpackInventory().isEmpty()){
-            return;
-        }
+        if(try_open_backpack && BackpackItem.isWearingIt(player)){
+            BackpackItem.openBackpackHandler(true, (ServerPlayerEntity) player);
+            try_open_backpack = false; // Shouldn't be necessary
+        }else {
 
-        if(open_inventory || BackpackItem.isWearingIt(player, getThis().getSlot(slotIndex).getStack())){
-            BackpackItem.openBackpackHandler(!open_inventory, (ServerPlayerEntity) player);
-            open_inventory = false; // Shouldn't be necessary
+            if (try_open_inventory && !((BackpackScreenHandler) getThis()).getBackpackInventory().isEmpty()) {
+                return;
+            }
+
+            if (try_open_inventory || BackpackItem.isWearingIt(player, getThis().getSlot(slotIndex).getStack())) {
+                BackpackItem.openBackpackHandler(!try_open_inventory, (ServerPlayerEntity) player);
+                try_open_inventory = false; // Shouldn't be necessary
+            }
+
         }
 
         if(getThis() instanceof BackpackScreenHandler){
