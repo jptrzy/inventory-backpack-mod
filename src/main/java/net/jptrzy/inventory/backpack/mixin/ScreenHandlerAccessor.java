76b2157ccd08cbbb97b59a6858d8e3d923c0ffd2
Jakub Trzeciak <jptrzy@gmail.com>
2022-01-13 22:36:51 +0100

    :wastebasket: Mixins

--- src/main/java/net/jptrzy/inventory/backpack/mixin/ScreenHandlerAccessor.java
+++ src/main/java/net/jptrzy/inventory/backpack/mixin/ScreenHandlerAccessor.java
@@ -9,9 +9,6 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(ScreenHandler.class)
 public interface ScreenHandlerAccessor {
-    @Accessor("type") @Mutable
-    void setType(ScreenHandlerType<?> type);
-
     @Accessor("syncId") @Mutable
     void setSyncId(int syncId);
 }
